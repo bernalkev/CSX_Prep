b8a6802a2b99114dc9d9ea8f3cbaca782e24423c
Kevin B <>
2024-07-04 18:44:39 -0600

    csx codesmith prep practice

--- eloquentjavascript.js/closures.js/closure1.js
+++ eloquentjavascript.js/closures.js/closure1.js
@@ -1,6 +1,30 @@
-// //// lexical scope
+// // //// lexical scope
 
-// // global scope
+// // // global scope
+
+// // let x = 1;
+
+// // const parentFunction = () => {
+// //     // local scope;
+// //     let myValue = 2
+// //     console.log(x);
+// //     console.log(myValue);
+
+// //     const childFunction = () => {
+// //         console.log(x += 5);
+// //         console.log(myValue += 1);
+// //     }
+
+// //     childFunction()
+// // }
+
+// // parentFunction()
+
+// // a closure is a function have access to the parent scope,    
+// // even after the parent function has closed
+
+// // a closure is created when we define a function, not when a function is 
+// // executed
 
 // let x = 1;
 
@@ -15,38 +39,42 @@
 //         console.log(myValue += 1);
 //     }
 
-//     childFunction()
+//     return childFunction;
 // }
 
-// parentFunction()
+// const result = parentFunction()
+// console.log(result);
 
-// a closure is a function have access to the parent scope,    
-// even after the parent function has closed
+// result();
+// result();
+// result();
+// console.log(x);
+// console.log(myValue);
 
-// a closure is created when we define a function, not when a function is 
-// executed
 
-let x = 1;
 
-const parentFunction = () => {
-    // local scope;
-    let myValue = 2
-    console.log(x);
-    console.log(myValue);
 
-    const childFunction = () => {
-        console.log(x += 5);
-        console.log(myValue += 1);
-    }
+/*
+Declare a recursive function "mapArray" that takes at least two arguments (an array and a callback function).
+"mapArray" should return a new array containing the returned values from running the callback function on each element in the array.
 
-    return childFunction;
+Once complete, write some code to test your 'mapArray' function.
+*/
+
+// function mapArray (array, callback)
+function mapArray (array, callback, newArr = []){
+    // base case where it exits
+   if(array.length === 0){
+      return newArr
+    } 
+    const transformedHead = callback(array[0]);
+    const transformedTail = mapArray(array.slice(1), callback)
+    return [transformedHead, ...transformedTail]
 }
+    // recursive function
+    	// return a new array containing the returned values from running teh callback function of each element in the array
 
-const result = parentFunction()
-console.log(result);
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = mapArray(numbers, (x) => x * 2);
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
 
-result();
-result();
-result();
-console.log(x);
-console.log(myValue);
